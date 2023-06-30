5dccf38c82d35bc1620083435d66b45eb2930886
Crowdbotics <team@crowdbotics.com>
2023-06-30 20:29:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mine-merchant-41834",
+  url: "https://mine-merchant-41834.botics.co",
+  api: "https://mine-merchant-41834.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
